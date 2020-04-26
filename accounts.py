4a29368312386f18e8a39113dc63ab7042e0dc76
Stanislaw Jaromin <jaromin@onet.pl>
2020-04-26 20:18:19 +0200

    konta bankowe

---
+++ accounts.py
@@ -0,0 +1,10 @@
+class Account:
+    """Simple account class with balance"""
+
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+        print("Account created for " + self.name)
+
+    def deposit(self, amount):
+        if amount >0
