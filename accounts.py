ec77a35af6e550910f3b015166497c5f09715824
Stanislaw Jaromin <jaromin@onet.pl>
2020-04-26 21:32:03 +0200

    konta bankowe

--- accounts.py
+++ accounts.py
@@ -1,21 +1,47 @@
+import datetime
+import pytz
+
+
 class Account:
     """Simple account class with balance"""
+
+    @staticmethod
+    def _current_time():
+        utc_time = datetime.datetime.utcnow()
+        return pytz.utc.localize(utc_time)
 
     def __init__(self, name, balance):
         self.name = name
         self.balance = balance
+        self.transaction_list = []
         print("Account created for " + self.name)
+        self.transaction_list.append((Account._current_time(), balance))
 
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount
+            self.show_balance()
+            self.transaction_list.append((Account._current_time(), amount))
 
     def withdraw(self, amount):
-        if amount > 0:
+        if 0 < amount <= self.balance:
             self.balance -= amount
+            self.transaction_list.append((Account._current_time(), -amount))
+        else:
+            print("The amount must be greater than zero and no more than your account balance")
+        self.show_balance()
 
     def show_balance(self):
         print("Balance is {}".format(self.balance))
+
+    def show_transactions(self):
+        for date, amount in self.transaction_list:
+            if amount > 0:
+                tran_type = "deposited"
+            else:
+                tran_type = "withdrawn"
+                amount *= -1
+            print("{:6} {} pm {} (local time was {}".format(amount, tran_type, date, date.astimezone()))
 
 
 if __name__ == '__main__':
@@ -23,7 +49,16 @@ if __name__ == '__main__':
     tim.show_balance()
 
     tim.deposit(1000)
-    tim.show_balance()
+    #tim.show_balance()
     tim.withdraw(500)
-    tim.show_balance()
+    #tim.show_balance()
+
+    tim.withdraw(3000)
+    tim.show_transactions()
+
+    stefek = Account("Stefek", 800)
+    stefek.deposit(100)
+    stefek.withdraw(200)
+    stefek.show_transactions()
+
 
