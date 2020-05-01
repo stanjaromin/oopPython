aa0958d67a05b49c1f347635e2280362e5743140
Stanislaw Jaromin <jaromin@onet.pl>
2020-05-01 22:26:54 +0200

    Player/main - omawiania geterów i setterów

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ tim = Player("Tim")
 print(tim.name)
 print(tim.lives)
 tim.lives -= 1
-print(tim.lives)
+print(tim)
 
+tim.lives -= 1
+print(tim)
+
+tim.lives -= 1
+print(tim)
+
+tim.lives -= 1
+print(tim)
 #print(tim.get_name())
 #tim.set_lives(300)
 
