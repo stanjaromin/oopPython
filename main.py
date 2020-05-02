cf4a7feb4eb34d3bc51bba74be9cb6dec202f662
Stanislaw Jaromin <jaromin@onet.pl>
2020-05-02 21:40:41 +0200

    Player/main - omawiania geterów i setterów

--- main.py
+++ main.py
@@ -22,6 +22,21 @@ print(tim)
 #print(tim.get_name())
 #tim.set_lives(300)
 
+ugly_troll = Troll("Pug")
+print("Ugly troll - {}".format(ugly_troll))
+
+another_troll = Troll("Ug")
+print("Another troll - {}".format(another_troll))
+
+brother = Troll("Urg")
+print(brother)
+
+ugly_troll.grunt()
+another_troll.grunt()
+brother.grunt()
+
+monster = Enemy("Basic enemy")
+monster.grunt()
 
 
 
